5e6b7708ea1125dde63325c34b0a39d9066f2c41
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-02-26 19:32:39 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170226230048) do
+
+  create_table "users", force: :cascade do |t|
+    t.string  "name"
+    t.integer "owner_id"
+  end
 
 end
